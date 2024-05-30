3ab1acbbb8728ff90800e8ce2d57cb54676f3244
SANKET MAHADIK <125791466+sanketmahadik191@users.noreply.github.com>
2024-05-30 10:33:23 +0530

    create header comp

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
-import { Route,Routes } from "react-router-dom";
+import { BrowserRouter, Route,Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import Signin from "./pages/Signin";
 import About from "./pages/About";
 import Signup from "./pages/Signup";
 import Profile from "./pages/Profile";
+import Header from "./components/Header";
 
 function App() {
   return (
+   <BrowserRouter>
+    <Header></Header>
     <Routes>
       <Route path="/" element={<Home />}></Route>
       <Route path="/sign-in" element={<Signin />}>Ai</Route>
@@ -15,6 +18,7 @@ function App() {
       <Route path="/about" element={<About />}></Route>
       <Route path="/profile" element={<Profile />}></Route>
     </Routes>
+    </BrowserRouter> 
   );
 }
 
