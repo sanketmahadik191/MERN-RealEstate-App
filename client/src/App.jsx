68514d022b73c921c9c1ac42d5f4a27ae15e8f5f
SANKET MAHADIK <125791466+sanketmahadik191@users.noreply.github.com>
2024-05-30 09:10:04 +0530

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,20 @@
-
+import { Route,Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import About from "./pages/About";
+import Signup from "./pages/Signup";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
-    <h1 className="text-red-500 bg-slate-200">HI</h1>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/sign-in" element={<Signin />}>Ai</Route>
+      <Route path="/sign-up" element={<Signup />}></Route>
+      <Route path="/about" element={<About />}></Route>
+      <Route path="/profile" element={<Profile />}></Route>
+    </Routes>
   );
 }
 
