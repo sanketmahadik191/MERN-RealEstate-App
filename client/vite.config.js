169100081d7a52f4801f358ae135c293d7b6b4da
SANKET MAHADIK <125791466+sanketmahadik191@users.noreply.github.com>
2024-06-14 12:03:37 +0530

    deployed port change

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server:{
+    port: 10000,
     proxy:{
       '/api':{
         target:'https://mern-realestate-app-1.onrender.com',
